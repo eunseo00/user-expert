6b211417fc80d424175c66bfdbea5f11b1268246
박은서 <eee@eee.com>
2019-09-03 22:02:55 +0900

    commit

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,22 +1,15 @@
 <template>
     <div>
-        <div v-for="user in users">{{ user.title }}</div>
+        <div v-for="user in this.$store.state.news">{{ user.title }}</div>
     </div>
 </template>
 
 <script>
-    import { fetchNewsList } from '../api/index';
+
 
     export default {
-        data() {
-            return {
-                users: []
-            }
-        },
         created() {
-            fetchNewsList()
-                .then(response => this.users = response.data)
-                .catch(error => console.log(error));
+            this.$store.dispatch('FETCH_NEWS');
         }
     }
 </script>
